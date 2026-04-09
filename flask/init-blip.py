77d24559136d86f3f3b1bdf6afe0be91ba8281f6
Patrick Carlson <carlson2442@gmail.com>
2026-04-08 23:05:59 -0500

    fix: unused variable removed

--- flask/init-blip.py
+++ flask/init-blip.py
@@ -2,7 +2,6 @@ import torch
 from transformers import BlipProcessor, BlipForConditionalGeneration
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
-torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
 # https://huggingface.co/Salesforce/blip-image-captioning-base
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
